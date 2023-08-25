3406b883be224abb3678ee5c36ce2a55acff888c
2kRohit <2krohitr@gmail.com>
2023-08-25 13:08:25 +0530

    2

--- student/student-frontend/src/App.js
+++ student/student-frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 import axios from 'axios';
 
 const classes = ['I', 'II', 'III', 'IV', 'V', 'V1', 'V11', 'V111', '1X', 'X', 'X11', 'X12'];
@@ -25,37 +24,31 @@ function App() {
   };
 
   const getdata = async () => {
+    try {
     const res = await axios.get('http://localhost:8080/students/list');
-    setStudents(res.data);
+    setStudents(res.data);}
+    catch (error) {
+      console.error('Error adding student:', error);
+    }
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      // Validate form data
-      if (
-        !formData.name ||
-        !formData.dob ||
-        !formData.className ||
-        !formData.division ||
-        !formData.gender
-      ) {
+      if (!formData.name || !formData.dob || !formData.className || !formData.division || !formData.gender) {
         alert('Please fill in all fields');
         return;
       }
 
-      // Send data to backend
       const response = await axios.post('http://localhost:8080/students/add', formData);
-      if(response.status===201){
-        alert('Student Information Successfully added')
+      if (response.status === 201) {
+        alert('Student Information Successfully added');
+      } else {
+        alert('Error occurred');
       }
-else{
-  alert('error occured')
-}
-      // Update student list
+
       getdata();
 
-      // Clear the form
       setFormData({
         name: '',
         dob: '',
@@ -73,87 +66,141 @@ else{
   }, []);
 
   return (
-    <div className="App">
-      <h1>Student Information Form</h1>
-      <form onSubmit={handleSubmit}>
-        <label>Name:</label>
-        <input type="text" name="name" value={formData.name} onChange={handleChange}
-         pattern="^[A-Za-z\s]+$" title="Only letters and spaces are allowed" />
-
-        <label>Date of Birth:</label>
-        <input
-          type="date"
-          name="dob"
-          value={formData.dob}
-          onChange={handleChange}
-          max={new Date().toISOString().split('T')[0]} // Restrict to past dates
-        />
-
-        <label>Class:</label>
-        <select name="className" value={formData.className} onChange={handleChange}>
-          <option value="">Select Class</option>
-          {classes.map((cls) => (
-            <option key={cls} value={cls}>
-              {cls}
-            </option>
-          ))}
-        </select>
-
-        <label>Division:</label>
-        <select name="division" value={formData.division} onChange={handleChange}>
-          <option value="">Select Division</option>
-          {divisions.map((division) => (
-            <option key={division} value={division}>
-              {division}
-            </option>
-          ))}
-        </select>
-
-        <label>Gender:</label>
-        {genders.map((gender) => (
-          <label key={gender}>
+    <div className='bg-sky-300 mt-0'>
+      <br></br>
+      <h1 className='text-4xl font-bold text-center '>Student Information</h1>
+    <div className=" py-12   flex justify-center items-center  ">
+      <div className="max-w-xl h-full w-full bg-sky-100 p-8  shadow-md rounded-md -mt-44 border-4 border-grey-100">
+        <h2 className="text-3xl font-semibold mb-6 text-center">Add Student Details</h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+              Name
+            </label>
             <input
-              type="radio"
-              name="gender"
-              value={gender}
-              checked={formData.gender === gender}
+              id="name"
+              name="name"
+              type="text"
+              autoComplete="off"
+              required
+              value={formData.name}
               onChange={handleChange}
+              className="appearance-none block w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-gray-900"
             />
-            {gender}
-          </label>
-        ))}
-
-        <button type="submit">Submit</button>
-      </form>
-
-      <h2>Student Details</h2>
-      <table>
-        <thead>
-          <tr>
-          <th>Admission Number</th>
-            <th>Name</th>
-            <th>Date of Birth</th>
-            <th>Class</th>
-            <th>Division</th>
-            <th>Gender</th>
-          </tr>
-        </thead>
-        <tbody>
-          {students
+          </div>
+          <div>
+            <label htmlFor="dob" className="block text-sm font-medium text-gray-700">
+              Date of Birth
+            </label>
+            <input
+              id="dob"
+              name="dob"
+              type="date"
+              required
+              max={new Date().toISOString().split('T')[0]}
+              value={formData.dob}
+              onChange={handleChange}
+              className="appearance-none block w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-gray-900"
+            />
+          </div>
+          <div>
+            <label htmlFor="className" className="block text-sm font-medium text-gray-700">
+              Class
+            </label>
+            <select
+              id="className"
+              name="className"
+              required
+              value={formData.className}
+              onChange={handleChange}
+              className="appearance-none block w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-gray-900"
+            >
+              <option value="">Select Class</option>
+              {classes.map((cls) => (
+                <option key={cls} value={cls}>
+                  {cls}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="division" className="block text-sm font-medium text-gray-700">
+              Division
+            </label>
+            <select
+              id="division"
+              name="division"
+              required
+              value={formData.division}
+              onChange={handleChange}
+              className="appearance-none block w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-gray-900"
+            >
+              <option value="">Select Division</option>
+              {divisions.map((division) => (
+                <option key={division} value={division}>
+                  {division}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">Gender</label>
+            <div className="mt-2 space-x-4">
+              {genders.map((gender) => (
+                <label key={gender} className="inline-flex items-center">
+                  <input
+                    type="radio"
+                    className="form-radio"
+                    name="gender"
+                    value={gender}
+                    checked={formData.gender === gender}
+                    onChange={handleChange}
+                  />
+                  <span className="ml-2 text-sm text-gray-700">{gender}</span>
+                </label>
+              ))}
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="max-w-full bg-sky-100 p-8 shadow-md rounded-md ml-6 -mt-8 border-4 border-grey-100">
+        <h2 className="text-3xl font-semibold mb-6 text-center">Student Details</h2>
+        <table className="w-full border">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">Admission Number</th>
+              <th className="px-4 py-2">Name</th>
+              <th className="px-4 py-2">Date of Birth</th>
+              <th className="px-4 py-2">Class</th>
+              <th className="px-4 py-2">Division</th>
+              <th className="px-4 py-2">Gender</th>
+            </tr>
+          </thead>
+          <tbody>
+            {students
             .sort((a, b) => a.name.localeCompare(b.name)) // Sort students by name
             .map((student, index) => (
               <tr key={index}>
-                <td>{student.admissionNumber}</td>
-                <td>{student.name}</td>
-                <td>{student.dob}</td>
-                <td>{student.className}</td>
-                <td>{student.division}</td>
-                <td>{student.gender}</td>
+                <td className="px-4 py-2">{student.admissionNumber}</td>
+                <td className="px-4 py-2">{student.name}</td>
+                <td className="px-4 py-2">{student.dob}</td>
+                <td className="px-4 py-2">{student.className}</td>
+                <td className="px-4 py-2">{student.division}</td>
+                <td className="px-4 py-2">{student.gender}</td>
               </tr>
             ))}
-        </tbody>
-      </table>
-    </div>
+          </tbody>
+        </table>
+      </div>
+    </div></div>
   );
 }
 
